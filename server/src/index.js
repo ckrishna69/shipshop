1f27cc661ea5a27afebd20f1c3a54a1d9deaede4
ckrishna69 <ckrishnasingh69@gmail.com>
2026-07-20 21:12:10 +0530

    Update CORS configuration

--- server/src/index.js
+++ server/src/index.js
@@ -13,16 +13,29 @@ import paymentRoutes from "./routes/payment.routes.js";
 
 const app = express();
 
+// --- REPLACE ONLY YOUR OLD CORS BLOCK WITH THIS ---
+const allowedOrigins = [
+  "https://shipshop-eight.vercel.app",
+  "http://localhost:5173",
+  "http://localhost:3000"
+];
+
 app.use(cors({
-  origin: [
-    "https://shipshop-eight.vercel.app",
-    "http://localhost:5173",
-    "http://localhost:3000"
-  ],
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"]
 }));
+
+app.options("*", cors());
+// --------------------------------------------------
+
 // Express middleware setup
 app.use(express.json());
 app.use(cookieParser());
